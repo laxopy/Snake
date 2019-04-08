6d44ab1b208480a40d6802122faaa8887940ac25
laxoPyGit <jlaxlopez@gmail.com>
2019-04-08 23:36:54 +0200

    añado más librerías

--- sketch.js
+++ sketch.js
@@ -15,8 +15,8 @@ function KeyPressed(){
     }else if(keyCode == DOWN_ARROW){
         s.dir=(0,1);
     }else if(keyCode == RIGHT_ARROW){
-        s.dir=(-1,0);
-    }else if(keyCode == LEFT_ARROW){
         s.dir=(1,0);
+    }else if(keyCode == LEFT_ARROW){
+        s.dir=(-1,0);
     }
 }
