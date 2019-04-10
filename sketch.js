e53ae4dc179f094e99428e504976b09c22b99de2
laxoPyGit <jlaxlopez@gmail.com>
2019-04-10 21:21:26 +0200

    subida

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ function draw(){
     s.show();
 }
 function KeyPressed(){
-    if(keyCode == UP_ARROW){
+    if(keyCode === UP_ARROW){
         s.dir=(0,-1);
-    }else if(keyCode == DOWN_ARROW){
+    }else if(keyCode === DOWN_ARROW){
         s.dir=(0,1);
-    }else if(keyCode == RIGHT_ARROW){
+    }else if(keyCode === RIGHT_ARROW){
         s.dir=(1,0);
-    }else if(keyCode == LEFT_ARROW){
+    }else if(keyCode === LEFT_ARROW){
         s.dir=(-1,0);
     }
 }
